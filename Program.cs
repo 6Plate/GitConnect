f16971fae36052c0d811cc8ec2f49542bfb0a713
PlatonD <drozenkop@gmail.com>
2023-07-30 18:26:49 +0500

    Создание главного метода

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] array1 = new string[4] {"Пекару", "23", "56", "world"};
+string[] array2 = new string[array1.Length];
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GitConnect
+1. Комми
